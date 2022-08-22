d93046846ca5724425e9ccd11b5f19f688440698
pedro mbote <mbotepm@gmail.com>
2022-08-22 12:02:29 +0100

    update getAllAgenda

--- Services/AgendaService.cs
+++ Services/AgendaService.cs
@@ -2,27 +2,36 @@ using api_farmacia.Models;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace api_farmacia.Services{
-    public class AgendaService{
+using Newtonsoft.Json;
 
-        public List<Agenda> getAllAgendas(IConfiguration configuration){
+namespace api_farmacia.Services
+{
+    public class AgendaService
+    {
+
+        public string getAllAgendas(IConfiguration configuration)
+        {
             List<Agenda> agendas = new List<Agenda>();
             string stringConexao = configuration["ConnectionStrings:DefaultConnection"];
-            using(SqlConnection conexao = new SqlConnection(stringConexao)){
+            using (SqlConnection conexao = new SqlConnection(stringConexao))
+            {
                 conexao.Open();
-                string sql = "select * from Agenda ag " + 
-                             "join Horario h            on ag.AgendaId             = h.AgendaId" + 
-                             "join ServicoFuncionario s on ag.ServicoFuncionarioId = ag.ServicoFuncionarioId";
-                using(SqlCommand cmd = new SqlCommand(sql,conexao)){
-                    using(SqlDataReader dataAdapter =  cmd.ExecuteReader()){
-                        while(dataAdapter.Read()){
-                            
-                        }
+                string sql = "select * from Agenda ag " +
+                             "join Horario h            on ag.AgendaId             = h.AgendaId" +
+                             "join ServicoFuncionario s on ag.ServicoFuncionarioId = ag.ServicoFuncionarioId" +
+                             "join Servico sc           on sc.ServicoId            = s.ServicoId" +
+                             "join Funcioario f         on f.FuncionarioId         = s.FuncionarioId" +
+                             "join Pessoa p             on p.PessoaId              = f.PessoaId";
+                using (SqlCommand cmd = new SqlCommand(sql, conexao))
+                {
+                    using (SqlDataReader dataAdapter = cmd.ExecuteReader())
+                    {
+                        string JSONString = string.Empty;
+                        JSONString = JsonConvert.SerializeObject(dataAdapter);
+                        return JSONString;
                     }
                 }
             }
-            return agendas;
-
         }
 
     }
